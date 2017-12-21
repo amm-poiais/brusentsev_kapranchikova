90d17d80a12f7d5819a8fc9301fd23e471c7d7e5
Kalter-M <ivanbrusentseb@gmail.com>
2017-12-21 10:02:22 +0300

    Model fix

--- JointTripApp/models.py
+++ JointTripApp/models.py
@@ -8,12 +8,10 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # путешественник - водитель/пассажир
 class Traveler(models.Model):
     # user_id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, default=1, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
-
-    trip_passenger = models.ManyToManyField('Trip', through='TripPassenger')
 
     def __str__(self):
         return 'Username: %s; email: %s' % (
@@ -38,13 +36,7 @@ class Trip(models.Model):
     smoke = models.BooleanField(default=False)
     pets = models.BooleanField(default=False)
 
-    passengers = models.ManyToManyField('Traveler', through='TripPassenger')
-
-
-# пассажиры в поездке
-class TripPassenger(models.Model):
-    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
-    passenger = models.ForeignKey(Traveler, on_delete=models.CASCADE)
+    passengers = models.ManyToManyField('Traveler')
 
 
 # отзыв
