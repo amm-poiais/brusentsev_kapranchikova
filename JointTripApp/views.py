4fc25dbae90689d9a731ee33491698ae2ef1d1a9
Kalter-M <ivanbrusentseb@gmail.com>
2017-12-27 10:09:22 +0300

    Add querySet in Test view

--- JointTripApp/views.py
+++ JointTripApp/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
-from django.template.context_processors import csrf
-from django.contrib import auth
-from django.shortcuts import render_to_response, redirect
-
 from .models import Traveler
 from .models import Trip
 from .models import Review
 
 
-# def travelers_list(request):
-#     travelers = Traveler.objects.order_by('surname')
-#     return render(request, 'test.html', {'travelers': travelers})
-
-
-def index(request):
-    trips = Trip.objects.all()
-    return render(request, 'JointTripApp/index.html', {
-        "trips": trips
-       # 'user': auth.get_user(request)
-    })
-
+def travelers_list(request):
+    travelers = Traveler.objects.order_by('surname')
+    return render(request, 'test.html', {'travelers': travelers})
