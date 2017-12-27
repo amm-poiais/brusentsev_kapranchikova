b14c0a17c772710a2e0358516c102cceb02e1b03
Alice Kapranchikova <kapranchikova_al@mail.ru>
2017-12-27 10:10:09 +0300

    Create index file

--- JointTrip/urls.py
+++ JointTrip/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from django.contrib import admin
+from JointTripApp import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('JointTripApp.urls')),
+    url('admin/', admin.site.urls),
+    url(r'', views.main),
 ]
--- JointTripApp/views.py
+++ JointTripApp/views.py
@@ -4,6 +4,9 @@ from .models import Trip
 from .models import Review
 
 
-def post_list(request):
-    travelers = Traveler.objects.order_by('surname')
-    return render(request, 'test.html', {'travelers': travelers})
+# def post_list(request):
+#     travelers = Traveler.objects.order_by('surname')
+#     return render(request, 'test.html', {'travelers': travelers})
+
+def main(request):
+    return render(request, 'JointTripApp/index.html')
