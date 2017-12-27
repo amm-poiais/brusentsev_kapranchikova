125940babd568f5c1f51ec759c2c1c8e8ea1aa96
Alice Kapranchikova <kapranchikova_al@mail.ru>
2017-12-27 20:26:43 +0300

    Add icons

--- JointTripApp/static/js/main.js
+++ JointTripApp/static/js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $('#find').click(function (event) {
         $.get("index.html", {date: $('#datepicker').val(), departure : $('#departure').val(), arrive: $('#arrive').val()})
             .done(function (data) {
-                alert("Data Loaded: " + data);
             });
     });
 
